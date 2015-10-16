0a311b864cecb61aea21e757cbcb3bab10fbf1db
Steve Jones <stevej@rand.org>
2015-10-16 13:48:38 -0400

    generated Dockerfiles

--- build-all.sh
+++ build-all.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
 
+clist="gen-dev haskell-dev:7.8.4 ocaml mirage haskell-dev:7.8.4-exercism ocaml-exercism mirage-exercism"
 
-for i in gen-dev haskell-dev:7.8.4 ocaml mirage exercism; do
+for i in $clist; do
+    echo
+    echo "Build $i"
+    echo
     cmd="docker build -t $i $i"
     if [ `uname -s` = 'Darwin' ]; then
         vgv $cmd
